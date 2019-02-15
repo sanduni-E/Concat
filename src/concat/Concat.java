6b838069f2f1741dd6cf54d7d5e0908dc127746f
sanduni-E <sanduniue@gmail.com>
2019-02-15 22:13:05 +0530

    Concat
    Concat

---
+++ src/concat/Concat.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package concat;
+
+/**
+ *
+ * @author DTC
+ */
+public class Concat {
+
+    
+    public static void main(String[] args) {
+        String quest="Whats your name?";
+        String answer="I'm bee.";
+        System.out.println(quest+answer+"This is how concat works.");
+    }
+    
+}
